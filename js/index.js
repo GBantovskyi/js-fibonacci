9c4d4218cf71cd7a828ec2aac6df600bac9e68ca
Grigorii Bantovskyi <gbantovsky@gmail.com>
2019-02-04 17:31:49 +0000

    created js

--- js/index.js
+++ js/index.js
@@ -4,14 +4,16 @@
 let number = parseInt(prompt("Enter Fibonacci number"));
 while (Number.isNaN(number)) {
     alert("You have to enter a number");
-    number = parseInt(prompt("Enter Fibonacci number"));
+    let number = parseInt(prompt("Enter Fibonacci number"));
 }
 
 function fibonacci(number) {
-    if (number >= 2) {
+    if (number <= 2) {
+        return 1;
+    }else{
 
-        return fibonacci(number - 1) + fibonacci(number - 2);
-    }
+    return fibonacci(number - 1) + fibonacci(number - 2);
+}
 }
 
 alert(fibonacci(number));
